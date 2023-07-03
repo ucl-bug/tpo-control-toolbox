37db8f3c7cea58de16b29724590e6dcc46b1a560
Bradley Treeby <b.treeby@ucl.ac.uk>
2023-07-03 15:07:41 +0100

    Expand TPO settings ranges

--- toolbox/getTPOSettings.m
+++ toolbox/getTPOSettings.m
@@ -1,23 +1,27 @@
 function settings = getTPOSettings()
-%POWEROFFTPO Turn TPO power off
+%GETTPOSETTINGS Define TPO and transducer specific settings.
 %
 % DESCRIPTION:
 %     getTPOSettings defines literals related to the TPO and the connected
 %     transducer. Edit this file to specify hardware specific parameters.
 %
 % OUTPUTS:
-%     settings      - struture with the following fields:
+%     settings      - structure with the following fields:
 %                       .number_channels
 %                       .maximum_power_value
 %                       .minimum_frequency
-%                       .maximum_frequency   
+%                       .maximum_frequency
+%                       .minimum_focus
+%                       .maximum_focus
 %
 % ABOUT:
 %     author        - Bradley Treeby
 %     date          - 29th November 2022
-%     last update   - 30th November 2022
+%     last update   - 3rd July 2023
 
-settings.number_channels        = 2;
+settings.number_channels        = 4;
 settings.maximum_power_value    = 30;
 settings.minimum_frequency      = 0.1e6;
 settings.maximum_frequency      = 10e6;
+settings.minimum_focus          = 33.6e-3;
+settings.maximum_focus          = 75.2e-3;
